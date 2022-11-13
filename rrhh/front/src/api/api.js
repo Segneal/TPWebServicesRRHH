9ebc5ed483d7673381008db7c754b9987ab4b5aa
Segneal <znicolasheredia@gmail.com>
2022-11-13 12:31:31 -0300

    sidebar options

--- rrhh/front/src/api/api.js
+++ rrhh/front/src/api/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
+const baseUrl = "http://localhost:3000/";
 
 export const getAllEstudiantes = async () => {
-  const route = "http://localhost:3000/usuario/getAllUsuarios";
+  const route = `${baseUrl}usuario/getAllUsuarios`;
+  const response = await axios.get(route);
+  console.log(response.data);
+  return response.data;
+};
+
+export const getAllCandidatos = async () => {
+  const route = `${baseUrl}candidato/getAllCandidatos`;
   const response = await axios.get(route);
   console.log(response.data);
   return response.data;
--- rrhh/front/src/components/Routes/BusquedaCandidatos.jsx
+++ rrhh/front/src/components/Routes/BusquedaCandidatos.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { getAllCandidatos } from "../../api/api";
 
 export default function BusquedaCandidatos() {
-  return <div>BusquedaCandidatos</div>;
+  const { data, error, loading } = useQuery("candidatos", getAllCandidatos, {
+    refetchOnWindowFocus: false,
+  });
+
+  const mostrarCandidatos = () => {
+    return data?.map((candidato) => {
+      return (
+        <div className="candidato" key={candidato.id}>
+          <p>{candidato.estado}</p>
+          <p>{candidato.usuario.nombre}</p>
+          <p>{candidato.usuario.apellido}</p>
+          <p>{candidato.usuario.carrera} </p>
+        </div>
+      );
+    });
+  };
+
+  return loading ? (
+    <div>Cargando...</div>
+  ) : error ? (
+    <div>Error</div>
+  ) : (
+    <div className="tabla-candidatos">{mostrarCandidatos()}</div>
+  );
 }
